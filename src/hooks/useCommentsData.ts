46ec760d7615446d14a12f3ba5e095e1813599dd
Evgeniy Kalimullin <cousnil1986@gmail.com>
2021-05-17 16:44:01 +0700

    homework mod.9

--- src/hooks/useCommentsData.ts
+++ src/hooks/useCommentsData.ts
@@ -2,39 +2,41 @@ import axios from 'axios'
 import { useContext, useEffect, useState } from 'react'
 import { tokenContext } from '../shared/context/tokenContext'
 
-interface IUseCommentsData {
-  name?: string
-  iconImg?: string
+interface IUseCommentData {
+  id?: string
+  author?: string
+  text?: string
+  created?: number
 }
 
 export function useCommentsData(id: string = '') {
-  const [data, setData] = useState<IUseCommentsData>({})
+  const [data, setData] = useState<IUseCommentData[]>([])
   const token = useContext(tokenContext)
-  console.log('id >>', id)
+
 
   useEffect(() => {
-    if (!!token) {
+    if (!!id) {
       axios
-        .get(`https://www.reddit.com/r/redditdev/comments/${id}`, {
-          headers: {
-            Authorization: `bearer ${token}`,
-            article: `${id}`,
-            depth: 1,
-            limit: 10,
-          },
-        })
+        .get(`https://www.reddit.com/comments/${id}.json?limit=10`)
         .then((resp) => {
-          console.log('resp >>>', resp)
-
-          const userData = resp.data
-          setData({
-            name: userData.name,
-            iconImg: userData.icon_img,
+          const commentsData = resp.data[1].data.children
+          console.log('>>', commentsData);
+          
+          const comments: IUseCommentData[] = commentsData
+          .filter((comment: any) => comment.kind === 't1')
+          .map((comment: any) => {
+            return {
+              id: comment.data.id ? comment.data.id : '',   
+              author: comment.data.author ? comment.data.author : '',
+              text: comment.data.body ? comment.data.body : '',
+              created: comment.data.created_utc ? comment.data.created_utc : 0
+            }
           })
+          setData(comments)
         })
         .catch(console.log)
     }
-  }, [token])
+  }, [id])
 
   return [data]
 }
--- src/shared/Post/Post.tsx
+++ src/shared/Post/Post.tsx
@@ -1,36 +1,46 @@
-import React, { useEffect, useRef } from 'react'
-import ReactDOM from 'react-dom'
-import { useCloseOnClickOut } from '../../hooks/useCloseOnClickOut'
-import { useCommentsData } from '../../hooks/useCommentsData'
-import { CommentForm } from './CommentForm'
-import { Comment } from './Comment'
-import styles from './post.css'
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import { useCloseOnClickOut } from "../../hooks/useCloseOnClickOut";
+import { useCommentsData } from "../../hooks/useCommentsData";
+import { CommentForm } from "./CommentForm";
+import { Comment } from "./Comment";
+import styles from "./post.css";
 
 interface IPostProps {
-  id?: string
-  title?: string
-  onClose: () => void
+  id?: string;
+  title?: string;
+  onClose: () => void;
 }
 
 export function Post({ id, title, onClose }: IPostProps) {
-  const [ref] = useCloseOnClickOut(onClose)
+  const [ref] = useCloseOnClickOut(onClose);
 
-  // const [comments] = useCommentsData(id)
+  const [comments] = useCommentsData(id);
+  console.log("comments", comments);
 
-  const node = document.querySelector('.modal_root')
-  if (!node) return null
+  const node = document.querySelector(".modal_root");
+  if (!node) return null;
   return ReactDOM.createPortal(
     <div className={styles.modal} ref={ref}>
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.content}>
-        <p>Есть над чем задуматься: тщательные исследования конкурентов представляют собой не что иное</p>
-        <p>Есть над чем задуматься: тщательные исследования конкурентов представляют собой не что иное</p>
-        <p>Есть над чем задуматься: тщательные исследования конкурентов представляют собой не что иное</p>
+        <p>
+          Есть над чем задуматься: тщательные исследования конкурентов
+          представляют собой не что иное
+        </p>
+        <p>
+          Есть над чем задуматься: тщательные исследования конкурентов
+          представляют собой не что иное
+        </p>
+        <p>
+          Есть над чем задуматься: тщательные исследования конкурентов
+          представляют собой не что иное
+        </p>
       </div>
       <CommentForm />
       {/* {comments.length > 0 &&
         comments.map((comment) => <Comment author={comment.author} text={comment.text} created={comment.created} />)} */}
     </div>,
     node
-  )
+  );
 }
