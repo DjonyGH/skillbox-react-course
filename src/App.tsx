2d4cc37098fab1d0ccd9ecbba9e6b59a430b6317
Evgeniy Kalimullin <cousnil1986@gmail.com>
2021-05-24 09:43:05 +0700

    homework mod.10

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import './main.global.css'
-import { hot } from 'react-hot-loader/root'
-import { Layout } from './shared/Layout/Layout'
-import { Header } from './shared/Header'
-import { Content } from './shared/Content'
-import { CardsList } from './shared/CardsList'
-import { useToken } from './hooks/useToken'
-import { tokenContext } from './shared/context/tokenContext'
-import { UserContextProvider } from './shared/context/userContext'
-import { PostsContextProvider } from './shared/context/postsContext'
-import { createStore } from 'redux'
-import { rootReducer, setToken } from './strore'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { Provider, useDispatch } from 'react-redux'
+import React, { useEffect } from "react";
+import "./main.global.css";
+import { hot } from "react-hot-loader/root";
+import { Layout } from "./shared/Layout/Layout";
+import { Header } from "./shared/Header";
+import { Content } from "./shared/Content";
+import { CardsList } from "./shared/CardsList";
+import { useToken } from "./hooks/useToken";
+import { tokenContext } from "./shared/context/tokenContext";
+import { UserContextProvider } from "./shared/context/userContext";
+import { PostsContextProvider } from "./shared/context/postsContext";
+import { createStore } from "redux";
+import { rootReducer, setToken } from "./strore";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider, useDispatch } from "react-redux";
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools());
 
 function AppComponent() {
-  const [token] = useToken()
-  const dispatch = useDispatch()
-  dispatch(setToken(token))
+  const [token] = useToken();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setToken(token));
+  }, [token]);
+
   return (
     <UserContextProvider>
       <PostsContextProvider>
@@ -31,7 +35,7 @@ function AppComponent() {
         </Layout>
       </PostsContextProvider>
     </UserContextProvider>
-  )
+  );
 }
 
 function AppComponentProvider() {
@@ -39,7 +43,7 @@ function AppComponentProvider() {
     <Provider store={store}>
       <AppComponent />
     </Provider>
-  )
+  );
 }
 
-export const App: React.FC = hot(() => <AppComponentProvider />)
+export const App: React.FC = hot(() => <AppComponentProvider />);
