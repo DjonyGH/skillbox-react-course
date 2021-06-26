fd70029eb6f95e6eec5596b8e250280abc54637e
Evgeniy Kalimullin <counsil1986@gmail.com>
2021-06-26 17:57:58 +0700

    homework mod.15

--- src/server/server.js
+++ src/server/server.js
@@ -24,7 +24,7 @@ app.get('/auth', (req, res) => {
   axios
     .post(
       'https://www.reddit.com/api/v1/access_token',
-      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://${process.env.REDIRECT}/auth`,
+      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.REDIRECT}`,
       {
         auth: { username: process.env.CLIENT_ID, password: process.env.SECRET },
         headers: { 'Content-type': 'application/x-www-form-urlencoded' },
